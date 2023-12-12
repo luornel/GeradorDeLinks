bb9ad974d01b67540fd82e4f7e01ed2d8c7ae535
Luã Ornelas <103165178+luornel@users.noreply.github.com>
2023-12-12 15:20:21 -0300

    Add files via upload

---
+++ GeradorDeLinksHTMLParaArquivosPDF.py
@@ -0,0 +1,29 @@
+import unidecode
+
+def generate_html_links(year, month, file_names):
+
+    html_links = ""
+    base_url = "//www.tjrs.jus.br/static/"
+
+    for file in file_names:
+        file_url = unidecode.unidecode(file).replace(' ', '-')
+        link = f'<li><a href="{base_url}{year}/{month}/{file_url}.pdf">{file}</a></li>\n'
+        html_links += link
+
+    return html_links
+
+# Como usar:
+year = input("Digite o ano: ")
+month = input("Digite o mês: ")
+file_names_input = input("Digite os nomes dos arquivos, separados por vírgula: ")
+
+file_names = [name.strip() for name in file_names_input.split(',')]
+
+html_output = generate_html_links(year, month, file_names)
+print(html_output)
+
+pip install unidecode
+
+pip install pyinstaller
+
+pyinstaller TestePy.py
